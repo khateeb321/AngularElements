802a8add9a6edbac26b31dce24fb071fb7a4d55d
Khateeb <khateeb321@gmail.com>
2019-03-27 14:42:49 +0100

    Moved API Call to Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { createCustomElement } from '@angular/elements';
 import { HttpClientModule } from '@angular/common/http';
 
 import { ButtonComponent } from './button/button.component';
+import { TestService } from './services/testService'
 
 @NgModule({
   declarations: [ButtonComponent],
   imports: [BrowserModule, HttpClientModule],
+  providers: [TestService],
   entryComponents: [ButtonComponent]
 })
 export class AppModule {
--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -5,7 +5,8 @@ import {
   EventEmitter,
   Output
 } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
+import { TestService } from '../services/testService';
 
 
 @Component({
@@ -29,18 +30,14 @@ export class ButtonComponent {
   @Output() action = new EventEmitter<any>();
   result: any;
   
-  constructor(private http: HttpClient){
+  constructor(private testService: TestService){
+
   }
 
   handleClick() {
-    this.apiCall().subscribe((data: any) => {
+    this.testService.getData().subscribe((data: any) => {
       this.result = data;
       this.action.emit(this.result);
     });
-  }
-
-  apiCall(){
-    // Dummy API call returns JSON
-    return this.http.get("https://jsonplaceholder.typicode.com/posts");
   }
 }
